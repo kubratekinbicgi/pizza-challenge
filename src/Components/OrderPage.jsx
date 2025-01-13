95f590986259615890c0f0ab230b75dc49a8f7fc
berkalparslan <aberkalparslan@gmail.com>
2025-01-13 21:21:58 +0300

    change order form props and valid states, add detail on confirm page

--- src/Components/OrderPage.jsx
+++ src/Components/OrderPage.jsx
@@ -8,46 +8,49 @@ import "./OrderForm.css"
 export default function OrderPage(){
 
     const [isOrderSubmitted, setIsOrderSubmitted] = useState(false); 
+    const [pizzaDetail, setPizzaDetail] = useState();
 
-    const handleOrderSubmit = () => {
-    setIsOrderSubmitted(true); 
-  };
+    const handleOrderSubmit = (detail) => {
+      console.log("🚀 ~ handleOrderSubmit ~ detail:", detail)
+      setPizzaDetail(detail);
+      setIsOrderSubmitted(true); 
+    };
 
     return(
         <div className="order-container">
-            {isOrderSubmitted ? (
-        <OrderConfirmation />
-      ) : (
-        <>
-            <div className="order-content">
-                <h1 className="order-title">Teknolojik Yemekler</h1>
-                <div className="nav-content">
-                <NavLink to="/" exact>
-                Anasayfa
-                </NavLink>
-                {"  -  "}
-                <NavLink to="/order">
-                Sipariş Oluştur
-                </NavLink>
+            {isOrderSubmitted && pizzaDetail ? (
+              <OrderConfirmation pizzaDetail={pizzaDetail} />
+            ) : (
+              <>
+                    <div className="order-content">
+                      <h1 className="order-title">Teknolojik Yemekler</h1>
+                      <div className="nav-content">
+                      <NavLink to="/" exact>
+                      Anasayfa
+                      </NavLink>
+                      {"  -  "}
+                      <NavLink to="/order">
+                      Sipariş Oluştur
+                      </NavLink>
+                    </div>
                 </div>
-            </div>
-        <div className="form-container">
-        <h2 className="pizza-name">Position Absolute Acı Pizza</h2>
-        <div className="price-content">
-        <h3 className="price">85.50₺</h3>
-        <h6 className="rating">4.9</h6>
-        
-        <h6 className="reviews">(200)</h6>
-        </div>
-        <p className="pizza-info">
-        Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
-        </p>
-       
-       
-        <OrderForm onSubmit={handleOrderSubmit}/>
-        </div>
-        </>
-      )}
+              <div className="form-container">
+              <h2 className="pizza-name">Position Absolute Acı Pizza</h2>
+              <div className="price-content">
+              <h3 className="price">85.50₺</h3>
+              <h6 className="rating">4.9</h6>
+              
+              <h6 className="reviews">(200)</h6>
+              </div>
+              <p className="pizza-info">
+              Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+              </p>
+            
+            
+              <OrderForm submitCallback={handleOrderSubmit}/>
+              </div>
+              </>
+            )}
     </div>
     )
 }
